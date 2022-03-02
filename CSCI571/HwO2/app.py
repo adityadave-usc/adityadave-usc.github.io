c983949a28268b1290638ce06d865317a71aff81
Aditya Dave <aditya.dave2017@gmail.com>
2022-03-01 16:34:38 -0800

    rectified vscode setup and python

--- CSCI571/HwO2/app.py
+++ CSCI571/HwO2/app.py
@@ -1,5 +1,3 @@
-
-
 from flask import Flask, Blueprint, request, jsonify
 from flask import render_template
 import pandas as pd
@@ -10,58 +8,50 @@ MODEL_PATH = "./regr.pkl"
 
 
 def create_app():
-  app = Flask(__name__)
-  app.config['MODEL_PATH'] = "./regr.pkl"
-  app.config['classifier'] = None
+    app = Flask(__name__)
+    app.config["MODEL_PATH"] = "./regr.pkl"
+    app.config["classifier"] = None
 
-  blood_pressure_predictor = Blueprint('blood_pressure_predictor', __name__, template_folder="templates", static_folder='static')
+    blood_pressure_predictor = Blueprint(
+        "blood_pressure_predictor", __name__, template_folder="templates", static_folder="static"
+    )
 
-  @app.before_first_request
-  def init_app():
-    app.config['classifier'] = joblib.load(app.config['MODEL_PATH'])
+    @app.before_first_request
+    def init_app():
+        app.config["classifier"] = joblib.load(app.config["MODEL_PATH"])
 
+    @blood_pressure_predictor.route("/", methods=["GET"])
+    def index():
+        """Return the main blood pressure prediction application"""
+        return render_template("index.html")
 
-  @blood_pressure_predictor.route("/", methods=["GET"])
-  def index():
-    """Return the main blood pressure prediction application
-    """
-    return render_template("index.html")
+    @blood_pressure_predictor.route("/predict", methods=["POST"])
+    def predict():
+        """Returns the predicted blood pressure from age and weight"""
+        clf = app.config["classifier"]
 
+        if clf is None:
+            print("Model not found!")
+            return jsonify({"status": "failed", "code": 404, "error": "Prediction model could not be found!"})
 
-  @blood_pressure_predictor.route("/predict", methods=["POST"])
-  def predict():
-    """Returns the predicted blood pressure from age and weight
-    """
-    clf = app.config['classifier']
-    
-    if clf is None:
-      print("Model not found!")
-      return jsonify({
-        'status': "failed",
-        "code": 404,
-        "error": "Prediction model could not be found!"
-      })
-    
-    req = request.json
-    age, weight = req['age'], req['weight']
+        req = request.json
+        age, weight = req["age"], req["weight"]
 
-    x = pd.DataFrame([[age, weight]], columns=["Age", "Weight"])
-    return jsonify({
-      'status': 'success',
-      'code': 200,
-      'request': {
-        'weight': weight,
-        'age': age
-      },
-      'result': {
-        'blood_pressure': clf.predict(x)[0]
-      }
-    })
+        x = pd.DataFrame([[age, weight]], columns=["Age", "Weight"])
+        return jsonify(
+            {
+                "status": "success",
+                "code": 200,
+                "request": {"weight": weight, "age": age},
+                "result": {"blood_pressure": clf.predict(x)[0]},
+            }
+        )
 
-  app.register_blueprint(blood_pressure_predictor, url_prefix="/bl00dPr6ssur6pr6d!ctor")
+    app.register_blueprint(blood_pressure_predictor, url_prefix="/bl00dPr6ssur6pr6d!ctor")
 
-  return app
+    return app
+
 
 if __name__ == "__main__":
-  app = create_app()
-  app.run()
+    app = create_app()
+    app.run()
